4783b8106bdf89a91489e11ef3f90e5047b75458
Vishal Choudhary <vishalchoudhary9770@gmail.com>
2026-05-26 17:43:02 +0530

    fix backend deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
-  res.send("Invoice Backend API is running");
+  res.send("Invoice backend is running");
 });
 
 app.use("/api/auth", authRoutes);
